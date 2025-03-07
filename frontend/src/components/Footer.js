582d0677c83323a9113f64acef7cb3f6fb8860f4
Sakshi <sakshibaghel302004@gmail.com>
2025-03-07 10:30:50 +0530

    Fixed frontend tracking and resolved submodule issue

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>Campus-Hunt</h1>
+                </Link>
+            </div>
+        </header>
+    )
+}
+export default Footer
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Navbar from '../components/Navbar'
+// import Footer from '../components/Footer'
+// import Signup from '../components/Signup'
+
+function Homepage() {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      {/* <Signup/> */}
+      {/* <Footer/> */}
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+export default Homepage
